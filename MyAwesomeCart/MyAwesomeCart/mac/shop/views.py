e5fcc23961ad9c7a32006c96bd0b2166e1128564
Harry-arora <99471403+Harry-arora@users.noreply.github.com>
2023-09-06 10:47:03 +0530

    Add files via upload

---
+++ MyAwesomeCart/MyAwesomeCart/mac/shop/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
+def index(request):
+    products= Product.objects.all()
+    n= len(products)
+    nSlides= n//4 + ceil((n/4) + (n//4))
+    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    return render(request,"shop/index.html", params)
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    return render(request,"shop/contact.html")
+
+def tracker(request):
+    return render(request,"shop/tracker.html")
+
+def search(request):
+    return render(request,"shop/search.html")
+
+def productView(request):
+    return render(request,"shop/productView.html")
+
+def checkout(request):
+    return render(request,"shop/checkout.html")
